411a2331459e0ea18b2d6e77e2fe90930358dd7d
enricorath <renrico@liberty.edu>
2024-09-19 17:43:22 +0800

    Add files via upload

--- get_ip.py
+++ get_ip.py
@@ -1,5 +1,7 @@
 import requests
+import ipaddress
 import os
+
 
 data={
 	'cloudflare':['https://monitor.gacjie.cn/api/client/get_ip_address?cdn_server=1&type=v4','https://monitor.gacjie.cn/api/client/get_ip_address?cdn_server=1&type=v6'],
@@ -24,7 +26,12 @@ for key,value in data.items():
 		for i in get_info_list:
 			proxy_ip=i['ip']
 			proxy_info=i['colo']
-			ips=ips+f'{proxy_ip}#{proxy_info}\n'
+			try:
+			    check_ip=ipaddress.IPv6Address(proxy_ip)
+			    proxy_ip=f'[{proxy_ip}]'
+			except Exception as e:
+				pass
+			ips=ips+f'{proxy_ip}:443#{proxy_info}\n'
 	print(ips)
 	with open(f'{key}-ip.txt','w') as file:
 		file.write(ips)
