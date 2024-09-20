cf56148ee6fc9c04b2d528808871820b493db6b8
enricorath <renrico@liberty.edu>
2024-09-20 14:43:56 +0800

    update ipdata

--- get_ip.py
+++ get_ip.py
@@ -1,29 +1,63 @@
 import requests
 import ipaddress
 import os
+import json
+import random
+import zipfile
+import re
+from pathlib import Path
 
 
-data={
-	'cloudflare':['https://monitor.gacjie.cn/api/client/get_ip_address?cdn_server=1&type=v4','https://monitor.gacjie.cn/api/client/get_ip_address?cdn_server=1&type=v6'],
-	'cloudfront':['https://monitor.gacjie.cn/api/client/get_ip_address?cdn_server=2&type=v4','https://monitor.gacjie.cn/api/client/get_ip_address?cdn_server=2&type=v6'],
-	'gcore':['https://monitor.gacjie.cn/api/client/get_ip_address?cdn_server=3&type=v4','https://monitor.gacjie.cn/api/client/get_ip_address?cdn_server=3&type=v6'],
-}
 headers={
 	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'
 }
-for key,value in data.items():
-	print(key)
+def all_cdn():
+	data={
+		'cloudflare':['https://monitor.gacjie.cn/api/client/get_ip_address?cdn_server=1&type=v4','https://monitor.gacjie.cn/api/client/get_ip_address?cdn_server=1&type=v6'],
+		'cloudfront':['https://monitor.gacjie.cn/api/client/get_ip_address?cdn_server=2&type=v4','https://monitor.gacjie.cn/api/client/get_ip_address?cdn_server=2&type=v6'],
+		'gcore':['https://monitor.gacjie.cn/api/client/get_ip_address?cdn_server=3&type=v4','https://monitor.gacjie.cn/api/client/get_ip_address?cdn_server=3&type=v6'],
+	}
+	for key,value in data.items():
+		print(key)
+		ips=''
+		for x in value:
+			get_info=requests.get(x,headers=headers).json()
+			get_cm=get_info['info']['CM']
+			get_cu=get_info['info']['CU']
+			get_ct=get_info['info']['CT']
+			get_info_list=get_cm+get_cu+get_ct
+			# print(get_info_list)
+			if get_info_list==[]:
+				break
+			for i in get_info_list:
+				proxy_ip=i['ip']
+				proxy_info=i['colo']
+				try:
+				    check_ip=ipaddress.IPv6Address(proxy_ip)
+				    proxy_ip=f'[{proxy_ip}]'
+				except Exception as e:
+					pass
+				ips=ips+f'{proxy_ip}:443#{proxy_info}\n'
+		print(ips)
+		with open(f'{key}-ip.txt','w') as file:
+			file.write(ips)
+			file.close()
+
+def get_cf_ip():
+	url='https://api.hostmonit.com/get_optimization_ip'
+	types=['v4','v6']
 	ips=''
-	for x in value:
-		get_info=requests.get(x,headers=headers).json()
-		get_cm=get_info['info']['CM']
-		get_cu=get_info['info']['CU']
-		get_ct=get_info['info']['CT']
-		get_info_list=get_cm+get_cu+get_ct
-		# print(get_info_list)
-		if get_info_list==[]:
-			break
-		for i in get_info_list:
+	for x in types:
+		data={'key': 'iDetkOys'}
+		headers['referer']='https://stock.hostmonit.com'
+		headers['access-control-allow-origin']='https://stock.hostmonit.com'
+		headers['origin']='https://stock.hostmonit.com'
+		headers['content-type']='application/json'
+		if x=='v6':
+			data['type']='v6'
+		ip_info=requests.post(url,data=json.dumps(data),headers=headers).json()['info']
+		# print(ip_info)
+		for i in ip_info:
 			proxy_ip=i['ip']
 			proxy_info=i['colo']
 			try:
@@ -32,6 +66,18 @@ for key,value in data.items():
 			except Exception as e:
 				pass
 			ips=ips+f'{proxy_ip}:443#{proxy_info}\n'
-	print(ips)
-	with open(f'{key}-ip.txt','w') as file:
-		file.write(ips)
+		print(ips)
+		is_tls=['443','80']
+		for x in is_tls:
+			if x=='443':
+				with open('cloudflare-ip1.txt','w') as file:
+					file.write(ips)
+			else:
+				ports=['80','2052','8080','2082','8880']
+				news_ips=ips.replace(':443',f':{random.choice(ports)}')
+				with open('cloudflare-ip1-notls.txt','w') as file:
+					file.write(news_ips)
+					file.close()
+		
+all_cdn()
+get_cf_ip()
---
+++ get_proxyip.py
@@ -0,0 +1,49 @@
+import requests
+import ipaddress
+import os
+import json
+import random
+import zipfile
+import re
+from pathlib import Path
+
+def get_cf_proxyip():
+	url='https://zip.baipiao.eu.org'
+	data=requests.get(url,headers=headers)
+	with open('cloudflare.zip','wb') as file:
+		file.write(data.content)
+		file.close()
+	with zipfile.ZipFile('cloudflare.zip', 'r') as zip_ref:
+		zip_ref.extractall('cf') 
+	# 目标文件夹路径
+	folder_path = Path('./cf')
+	# 遍历文件夹中的所有文件
+	tls_json=[]
+	notls_json=[]
+	for file_path in folder_path.rglob('*'):
+	    ports=re.split(r'-',file_path.stem)[-1]
+	    with open(file_path,'r',encoding='utf-8') as file:
+	    	text=file.read()
+	    	# news_text=text.replace('\n',f':{ports}\n')
+	    	news_text=re.split(r'\n',text)
+	    	if ports=='443':
+	    		for i in news_text:
+	    			ip_info=requests.get(f'https://ipinfo.io/{i}/json').json()
+	    			tls_json.append({'ip':i,'port':ports,'colo':f'{ip_info['country']}-{ip_info['city']}-{ip_info['org']}'})
+	    	else:
+	    		for i in news_text:
+	    			ip_info=requests.get(f'https://ipinfo.io/{i}/json').json()
+	    			tls_json.append({'ip':i,'port':ports,'colo':f'{ip_info['country']}-{ip_info['city']}-{ip_info['org']}'})
+	    	file.close()
+	    print(f'{ports}\n{text}')
+	file_info={'cloudflare-proxyip':json1,'cloudflare-proxyip-notls':json2}
+	for filename,ip_info in file_info:
+		ips=''
+		for j in ip_info:
+			ips=ips+f'{j['ip']}:{j['port']}#{j[colo]}'
+		with open(f'{filename}.txt','w') as file:
+			file.write(ips)
+			file.close()
+	os.remove('./cloudflare.zip')
+	os.remkdir('./cf')
+get_cf_proxyip()
